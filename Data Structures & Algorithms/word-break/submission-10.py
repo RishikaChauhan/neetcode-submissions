027a7dd5422246aa52e7f73c96d6f6d3ecab4afb
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2026-05-03 09:41:28 +0530

    Add: word-break - submission-10

---
+++ Data Structures & Algorithms/word-break/submission-10.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        d = {}
+        def dfs(s):
+            if s in d:
+                return d[s]
+            if s in wordSet:
+                d[s] = True
+                return True
+            for i in range(len(s)):
+                s1 = s[:i]
+                s2 = s[i:]
+                if s1 in wordSet:
+                    
+
+                    if dfs(s2):
+                        d[s2]=True
+                        return True
+            d[s] = False
+            return False
+        return dfs(s)
