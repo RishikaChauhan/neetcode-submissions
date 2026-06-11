04f60b9bf54a02c8fbb2b96f1cdf13a18e68c211
RishikaChauhan <60258506+RishikaChauhan@users.noreply.github.com>
2026-06-11 11:42:36 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        # for i in range(len(t), len(s)+1):
+        #     for j in range(len(s)-i+1):
+        #         ss = s[j:j+i]
+        #         if set(t).intersection(set(ss)) == set(t):
+        #             valid = True
+        #             for ch in t:
+        #                 if ss.count(ch) < t.count(ch):
+        #                     valid = False
+        #                     break
+        #             if valid:
+        #                 return ss
+                    
+        # return ""
+
+        for i in range(len(t), len(s)+1):
+            for j in range(len(s)-i+1):
+                ss = s[j:j+i]
+                if set(t).intersection(set(ss))==set(t):
+                    valid = True
+                    for ch in t:
+                        if ss.count(ch)<t.count(ch):
+                            valid = False
+                            break
+                    if valid :
+                        return ss
+        return ""
